9d40005a22999cfe228a9641c47e5e9ea5cd9cfe
Zack Watson <zackwatson.qut@gmail.com>
2017-05-22 21:51:20 +1000

    Initial Committ

---
+++ ContactApp/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ContactApp
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/lib/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/js/lib/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/js/lib/jquery.dataTables.min.js",
+                         "~/js/lib/dataTables.bootstrap.min"
+                        ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap.min.css",
+                     // "~/css/bootstrap-theme.min.css",
+                      "~/css/jquery.dataTables.min.css",
+                      "~/css/jquery.dataTables_themeroller.css",
+                      "~/css/dataTables.bootstrap.min.css",
+                      "~/css/colReorder.bootstrap.min.css",
+                      "~/css/site.css"));
+        }
+    }
+}
---
+++ ContactApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactApp.Startup))]
+namespace ContactApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
